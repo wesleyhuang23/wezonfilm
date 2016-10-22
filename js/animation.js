a4658bebd00a48977f80a336abae73505d4a32b1
wesleyhuang23 <wesleyhuang23@gmail.com>
2016-10-22 10:24:01 -0600

    closing side menu

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
   $('.side-menu').hide();
+  /*opening side menu*/
   $('.hamburger').on('click', function(){
     $('.side-menu').show();
   });
+
+  /*closing side menu*/
+  $('.cancel').on('click', function(){
+    $('.side-menu').hide();
+  });
+
 });
