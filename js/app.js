ff17c510eb4fcaeda9a8f5331937994c88454cc4
wesleyhuang23 <wesleyhuang23@gmail.com>
2016-10-30 14:10:00 -0600

    ui-routing working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-angular.module('wezApp', []);
+
+angular.module('wezApp', ['ui.router'])
+
+.config(function($urlRouterProvider, $stateProvider){
+
+  $stateProvider
+  .state('home', {
+    templateUrl: 'js/home/homeTmpl.html',
+    url: '/home', //url for when you search in the bar
+    controller: 'filmCtrl'
+  })
+  .state('library', {
+    templateUrl: 'js/library/libraryTmpl.html',
+    url: '/library',
+    controller: 'filmCtrl'
+  });
+
+  $urlRouterProvider.otherwise('/home');
+
+});
--- js/service.js
+++ js/service.js
@@ -6,7 +6,7 @@ var films = [
 "link":"reviews/amanda-knox-10-20-16.html"},
 {"Title":"St. Vincent","Year":"2014",
 "Poster":"https://images-na.ssl-images-amazon.com/images/M/MV5BMTk5NzI5OTA4MF5BMl5BanBnXkFtZTgwNjc4NTM3MjE@._V1_SX300.jpg"},
-{"Title":"Come Drink with Me","Year":"1966","Poster":"http://ia.media-imdb.com/images/M/MV5BNjczOTQzODQtNGVlZS00ODJlLWEzNWUtZDkxNmJhNzBkYTIwXkEyXkFqcGdeQXVyNDAzMzgyMTI@._V1_SX300.jpg",
+{"Title":"Come Drink with Me","Year":"1966","Poster":"http://36.media.tumblr.com/589bc5d311d8d3e41587872aaae660a3/tumblr_nfs02fP10c1qimoe7o4_1280.jpg",
 "link":"reviews/come-drink-with-me-2016-10-22.html"
 },
 {"Title":"The Kingdom of Dreams and Madness","Year":"2013",
